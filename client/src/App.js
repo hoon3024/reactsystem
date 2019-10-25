e57453beba60ea12b99fa51af15f01fa622d6c41
changhoon <hoon3024@naver.com>
2019-10-26 02:53:12 +0900

    CustomerAdd

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,36 @@
 import React, { Component } from 'react';
-
 import Customer from './components/Customer'
-
 import './App.css';
-
 import Paper from '@material-ui/core/Paper';
-
 import Table from '@material-ui/core/Table';
-
 import TableHead from '@material-ui/core/TableHead';
-
 import TableBody from '@material-ui/core/TableBody';
-
 import TableRow from '@material-ui/core/TableRow';
-
 import TableCell from '@material-ui/core/TableCell';
-
 import CircularProgress from '@material-ui/core/CircularProgress';
-
 import { withStyles } from '@material-ui/core/styles';
-
+import CustomerAdd from './components/CustomerAdd';
 
 
 const styles = theme => ({
 
-root: {
+    root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+    },
 
-width: "100%",
+    table: {
 
-marginTop: theme.spacing.unit * 3,
+        minWidth: 1080
 
-overflowX: "auto"
+        },
 
-},
+    progress: {
 
-table: {
+        margin: theme.spacing.unit * 2
 
-minWidth: 1080
-
-},
-
-progress: {
-
-margin: theme.spacing.unit * 2
-
-}
+        }          
 
 });
 
@@ -54,33 +40,33 @@ class App extends Component {
 
 state = {
 
-customers: '',
+    customers: '',
 
-completed: 0
+    completed: 0
 
-}
+    }
 
 
 
 componentDidMount() {
 
-this.timer = setInterval(this.progress, 20);
+    this.timer = setInterval(this.progress, 20);
 
-this.callApi()
+    this.callApi()
 
-.then(res => this.setState({customers: res}))
+    .then(res => this.setState({customers: res}))
 
-.catch(err => console.log(err));
+    .catch(err => console.log(err));
 
-}
+    }
 
 
 
 componentWillUnmount() {
 
-clearInterval(this.timer);
+    clearInterval(this.timer);
 
-}
+    }
 
 
 
@@ -108,52 +94,52 @@ progress = () => {
 
 render() {
 
-const { classes } = this.props;
+    const { classes } = this.props;
 
-return (
+    return (
+    <div>
+    <Paper className={classes.root}>
 
-<Paper className={classes.root}>
+    <Table className={classes.table}>
 
-<Table className={classes.table}>
+    <TableHead>
 
-<TableHead>
+    <TableRow>
 
-<TableRow>
+        <TableCell>번호</TableCell>
 
-<TableCell>번호</TableCell>
+        <TableCell>이미지</TableCell>
 
-<TableCell>이미지</TableCell>
+        <TableCell>이름</TableCell>
 
-<TableCell>이름</TableCell>
+        <TableCell>생년월일</TableCell>
 
-<TableCell>생년월일</TableCell>
+        <TableCell>성별</TableCell>
 
-<TableCell>성별</TableCell>
+        <TableCell>직업</TableCell>
 
-<TableCell>직업</TableCell>
+    </TableRow>
 
-</TableRow>
+    </TableHead>
 
-</TableHead>
+    <TableBody>
 
-<TableBody>
+        {this.state.customers ? this.state.customers.map(c => {
 
-    {this.state.customers ? this.state.customers.map(c => {
+            return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
 
-return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+         }) :
 
-}) :
+    <TableRow>
 
-<TableRow>
+         <TableCell colSpan="6" align="center">
+        
 
-<TableCell colSpan="6" align="center">
-    
+          <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
 
-<CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+         </TableCell>
 
-</TableCell>
-
-</TableRow>
+    </TableRow>
 
 }
 
@@ -162,7 +148,8 @@ return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.
 </Table>
 
 </Paper>
-
+<CustomerAdd/>
+</div>
 );
 
 }
